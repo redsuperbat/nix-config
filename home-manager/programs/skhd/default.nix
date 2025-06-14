3cfbfd8c136f9292a2bd5ca8f58d999aa9117d8b
Max Netterberg <max.netterberg@gmail.com>
2025-06-14 14:15:18 +0200

    adding ghostty to skhd

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -1,9 +1,11 @@
 {pkgs, ...}: {
   services.skhd = {
     enable = pkgs.stdenv.isDarwin;
+    # Since Ghostty is currently installed with hombrew we cannot reference it
+    # through the store
     config = ''
       cmd - 1 : open -a "${pkgs.brave}/Applications/Brave Browser.app"
-
+      cmd - 2 : open -a "/Applications/Ghostty.app"
       cmd - 3 : open -a "${pkgs.slack}/Applications/Slack.app"
     '';
   };

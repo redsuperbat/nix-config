1b11476c9be866134787520cc8dbf9a473379e52
Max Netterberg <max.netterberg@gmail.com>
2025-06-17 22:09:11 +0200

    Remove unused config

--- home-manager/programs/ghostty/default.nix
+++ home-manager/programs/ghostty/default.nix
@@ -6,16 +6,12 @@
         macos-non-native-fullscreen = true
         fullscreen = true
         window-padding-y = 10,0
-        command = ${pkgs.bash}/bin/bash -cl 'fish -c "tmux a"'
+        initial-command = ${pkgs.bash}/bin/bash -cl 'fish -c "tmux a"'
         font-family = JetBrains Mono
         font-family = Symbols Nerd Font Mono
-
         theme = Kanagawa Wave
-
         keybind = alt+shift+e=toggle_fullscreen
         keybind = alt+shift+r=reload_config
-        keybind = global:unconsumed:alt+j=unbind
-        keybind = global:unconsumed:alt+k=unbind
       '';
     };
   };

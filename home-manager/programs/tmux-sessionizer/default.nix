61f48cbe97baa896c442310b05e640d50af8a5df
Max Netterberg <max.netterberg@gmail.com>
2025-06-17 16:49:06 +0200

    Forward config and workspace dir to tms

--- home-manager/programs/tmux-sessionizer/default.nix
+++ home-manager/programs/tmux-sessionizer/default.nix
@@ -1,15 +1,19 @@
-{...}: {
+{
+  configDir,
+  workspaceDir,
+  ...
+}: {
   xdg.configFile = {
     "tms/config.toml" = {
       text = ''
         display_full_path = true
         session_sort_order = "LastAttached"
         [[search_dirs]]
-        path = "~/Config"
-        depth = 2
+        path = "${configDir}"
+        depth = 5
         [[search_dirs]]
-        path = "~/Workspace"
-        depth = 2
+        path = "${workspaceDir}"
+        depth = 5
       '';
     };
   };

a60374b2f3d39e4b9f4383ed436b29f71636d835
Max Netterberg <max.netterberg@gmail.com>
2025-06-17 16:47:13 +0200

    Remove unused comment

--- home-manager/programs/bat/default.nix
+++ home-manager/programs/bat/default.nix
@@ -5,7 +5,7 @@
       kanagawa = {
         src = pkgs.fetchFromGitHub {
           owner = "rebelot";
-          repo = "kanagawa.nvim"; # Bat uses sublime syntax for its themes
+          repo = "kanagawa.nvim";
           rev = "debe91547d7fb1eef34ce26a5106f277fbfdd109";
           sha256 = "sha256-i54hTf4AEFTiJb+j5llC5+Xvepj43DiNJSq0vPZCIAg=";
         };

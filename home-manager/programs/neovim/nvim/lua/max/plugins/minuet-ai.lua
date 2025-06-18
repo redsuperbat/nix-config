185f4291e40c8b78da7d6e3da3a48218963015cc
Max Netterberg <max.netterberg@gmail.com>
2025-06-18 14:58:35 +0200

    Add minuet-ai

---
+++ home-manager/programs/neovim/nvim/lua/max/plugins/minuet-ai.lua
@@ -0,0 +1,33 @@
+local file_path = vim.fn.expand("~/.secrets/openai-api-key")
+
+---@module "lazy"
+---@type LazySpec
+return {
+  "milanglacier/minuet-ai.nvim",
+  enabled = function()
+    return vim.fn.filereadable(file_path) == 1
+  end,
+  event = "VeryLazy",
+  config = function()
+    require("minuet").setup({
+      provider = "openai",
+      virtualtext = {
+        auto_trigger_ft = { "*" },
+        keymap = {
+          accept = "<Tab>",
+          prev = "<C-p>",
+          next = "<C-n>",
+          dismiss = "<C-e>",
+        },
+      },
+      provider_options = {
+        openai = {
+          api_key = function()
+            local lines = vim.fn.readfile(file_path)
+            return vim.trim(table.concat(lines, "\n"))
+          end,
+        },
+      },
+    })
+  end,
+}

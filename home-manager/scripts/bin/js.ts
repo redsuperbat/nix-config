0f9cb04770874d0f986068513106c92b55a93fbc
Max Netterberg <max.netterberg@gmail.com>
2025-07-01 22:50:31 +0200

    Refactor js.ts

--- home-manager/scripts/bin/js.ts
+++ home-manager/scripts/bin/js.ts
@@ -1,30 +1,23 @@
 #!/usr/bin/env deno -A
 import process from "node:process";
 import { createRequire } from "node:module";
-import util from "node:util";
+import { inspect } from "node:util";
 import { runInNewContext } from "node:vm";
 
 function readStdin() {
-	if (process.stdin.isTTY) return "";
 	return new Promise<string>((resolve, reject) => {
 		let input = "";
+		if (process.stdin.isTTY) return resolve(input);
 		process.stdin.setEncoding("utf8");
-		process.stdin.on("data", (chunk: string) => {
-			input += chunk;
-		});
+		process.stdin.on("data", (chunk: string) => (input += chunk));
 		process.stdin.on("end", () => resolve(input));
 		process.stdin.on("error", (err: Error) => reject(err));
 	});
 }
 
 const stdin = await readStdin();
-const data = eval(stdin);
-const program = process.argv[2];
-const result = runInNewContext(program, {
-	data,
+const result = runInNewContext(process.argv[2], {
+	data: eval(stdin),
 	require: createRequire(import.meta.url),
 });
-
-process.stdout.write(
-	util.inspect(result, { depth: null, maxArrayLength: null }),
-);
+process.stdout.write(inspect(result, { depth: null, maxArrayLength: null }));

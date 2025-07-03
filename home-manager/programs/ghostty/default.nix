527b76a684af1b0654ccb963cfd9251e6bd14f6d
Max Netterberg <max.netterberg@gmail.com>
2025-07-03 13:51:43 +0200

    Flatten config and fix opencode theme

--- home-manager/programs/ghostty/default.nix
+++ home-manager/programs/ghostty/default.nix
@@ -1,18 +1,14 @@
 {pkgs, ...}: {
-  xdg.configFile = {
-    "ghostty/config" = {
-      text = ''
-        macos-titlebar-style = hidden
-        macos-non-native-fullscreen = true
-        fullscreen = true
-        window-padding-y = 10,0
-        command = ${pkgs.bash}/bin/bash -cl 'fish -c "tmux a"'
-        font-family = JetBrains Mono
-        font-family = Symbols Nerd Font Mono
-        theme = Kanagawa Wave
-        keybind = alt+shift+e=toggle_fullscreen
-        keybind = alt+shift+r=reload_config
-      '';
-    };
-  };
+  xdg.configFile."ghostty/config".text = ''
+    macos-titlebar-style = hidden
+    macos-non-native-fullscreen = true
+    fullscreen = true
+    window-padding-y = 10,0
+    command = ${pkgs.bash}/bin/bash -cl 'fish -c "tmux a"'
+    font-family = JetBrains Mono
+    font-family = Symbols Nerd Font Mono
+    theme = Kanagawa Wave
+    keybind = alt+shift+e=toggle_fullscreen
+    keybind = alt+shift+r=reload_config
+  '';
 }

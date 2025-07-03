196b9d6abf6a1e4e404c4453a4bdb9975d0c1a68
Max Netterberg <max.netterberg@gmail.com>
2025-07-03 13:39:37 +0200

    Add opencode config

---
+++ home-manager/programs/opencode/default.nix
@@ -0,0 +1,3 @@
+{...}: {
+  xdg.configFile."opencode/config.json".source = ./opencode-config.json;
+}
--- home-manager/programs/tmux-sessionizer/default.nix
+++ home-manager/programs/tmux-sessionizer/default.nix
@@ -3,18 +3,14 @@
   workspaceDir,
   ...
 }: {
-  xdg.configFile = {
-    "tms/config.toml" = {
-      text = ''
-        display_full_path = true
-        session_sort_order = "LastAttached"
-        [[search_dirs]]
-        path = "${configDir}"
-        depth = 5
-        [[search_dirs]]
-        path = "${workspaceDir}"
-        depth = 5
-      '';
-    };
-  };
+  xdg.configFile."tms/config.toml".text = ''
+    display_full_path = true
+    session_sort_order = "LastAttached"
+    [[search_dirs]]
+    path = "${configDir}"
+    depth = 5
+    [[search_dirs]]
+    path = "${workspaceDir}"
+    depth = 5
+  '';
 }

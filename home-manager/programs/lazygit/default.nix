39b841631c05c84579e14dc0e2da5d97c8811094
Max Netterberg <max.netterberg@gmail.com>
2025-07-28 13:05:04 +0200

    Add lnav

--- home-manager/programs/lazygit/default.nix
+++ home-manager/programs/lazygit/default.nix
@@ -1,8 +1,6 @@
 {...}: {
-  # Install lazygit via home-manager module
   programs.lazygit = {
     enable = true;
-
     settings = {
       promptToReturnFromSubprocess = false;
       git.paging.pager = "delta --paging=never";

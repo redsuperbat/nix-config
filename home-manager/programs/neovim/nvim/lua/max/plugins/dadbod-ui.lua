86b7dd2a1e37d4faa6e79d94d101700a64a731ab
Max Netterberg <max.netterberg@gmail.com>
2025-08-21 17:18:06 +0200

    Disable dadbod

--- home-manager/programs/neovim/nvim/lua/max/plugins/dadbod-ui.lua
+++ home-manager/programs/neovim/nvim/lua/max/plugins/dadbod-ui.lua
@@ -2,6 +2,7 @@
 ---@type LazySpec
 return {
   "kristijanhusak/vim-dadbod-ui",
+  enabled = false,
   dependencies = {
     { "tpope/vim-dadbod", lazy = true },
     { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },

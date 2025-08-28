9c08a1fd98666c26bb7a6a6719441e9d508b0099
Max Netterberg <max.netterberg@gmail.com>
2025-08-28 18:50:36 +0200

    change mappings of skhd

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -4,9 +4,9 @@
     # Since Ghostty is currently installed with hombrew we cannot reference it
     # through the store
     config = ''
-      cmd - 1 : open -a "${pkgs.brave}/Applications/Brave Browser.app"
-      cmd - 2 : open -a "/Applications/Ghostty.app"
-      cmd - 3 : open -a "${pkgs.slack}/Applications/Slack.app"
+      cmd - h : open -a "${pkgs.brave}/Applications/Brave Browser.app"
+      cmd - j : open -a "/Applications/Ghostty.app"
+      cmd - k : open -a "${pkgs.slack}/Applications/Slack.app"
     '';
   };
 }

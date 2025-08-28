289d01a7bed79709eb951c665b22c5c8b35a659b
Max Netterberg <max.netterberg@gmail.com>
2025-08-28 19:40:58 +0200

    remap again

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -4,9 +4,9 @@
     # Since Ghostty is currently installed with hombrew we cannot reference it
     # through the store
     config = ''
-      cmd - h : open -a "${pkgs.brave}/Applications/Brave Browser.app"
-      cmd - j : open -a "/Applications/Ghostty.app"
-      cmd - k : open -a "${pkgs.slack}/Applications/Slack.app"
+      cmd - j : open -a "${pkgs.brave}/Applications/Brave Browser.app"
+      cmd - k : open -a "/Applications/Ghostty.app"
+      cmd - l : open -a "${pkgs.slack}/Applications/Slack.app"
     '';
   };
 }

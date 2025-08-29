06e184752b2d53b39fe3ae3136a29804b7f37d72
Max Netterberg <max.netterberg@gmail.com>
2025-08-29 22:22:48 +0200

    add function keys for popular programs

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -4,9 +4,9 @@
     # Since Ghostty is currently installed with hombrew we cannot reference it
     # through the store
     config = ''
-      cmd - j : open -a "${pkgs.brave}/Applications/Brave Browser.app"
-      cmd - k : open -a "/Applications/Ghostty.app"
-      cmd - l : open -a "${pkgs.slack}/Applications/Slack.app"
+      f1 : open -a "${pkgs.brave}/Applications/Brave Browser.app"
+      f2 : open -a "/Applications/Ghostty.app"
+      f3 : open -a "${pkgs.slack}/Applications/Slack.app"
     '';
   };
 }

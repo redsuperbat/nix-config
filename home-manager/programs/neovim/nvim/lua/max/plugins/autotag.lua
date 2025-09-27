e4ffc830f3573be7ba568b9913eb7c9c72dc1710
Max Netterberg <max.netterberg@gmail.com>
2025-09-27 22:30:35 +0200

    fix warning message in autopairs

--- home-manager/programs/neovim/nvim/lua/max/plugins/autotag.lua
+++ home-manager/programs/neovim/nvim/lua/max/plugins/autotag.lua
@@ -4,8 +4,13 @@ return {
   "windwp/nvim-ts-autotag",
   event = "InsertEnter",
   opts = {
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = true, -- Auto close on trailing </
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = true,
+    },
+    per_filetype = {
+      html = { enable_close = false },
+    },
   },
 }

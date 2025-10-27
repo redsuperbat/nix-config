d367dd88c7f120fb9b9503ca71386a671470a751
Max Netterberg <max.netterberg@gmail.com>
2025-10-27 08:26:08 +0100

    fix golang

--- home-manager/programs/go/default.nix
+++ home-manager/programs/go/default.nix
@@ -1,15 +1,4 @@
 {...}: {
   # Install and configure Golang via home-manager module
-  programs.go = {
-    enable = true;
-    env = {
-      GOBIN = "go/bin";
-      GOPATH = "go";
-    };
-  };
-
-  # Ensure Go bin in the PATH
-  home.sessionPath = [
-    "$HOME/go/bin"
-  ];
+  programs.go.enable = true;
 }

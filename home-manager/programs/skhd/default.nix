471a72b93eebc30d643d0b3c6e8084c17d6580f1
Max Netterberg <max.netterberg@gmail.com>
2025-12-19 08:13:38 +0100

    fix paths

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -1,12 +1,11 @@
 {pkgs, ...}: {
   services.skhd = {
     enable = pkgs.stdenv.isDarwin;
-    # Since Ghostty is currently installed with hombrew we cannot reference it
-    # through the store
+    # Some programs are installed with homebrew and therefore we cannot reference them through the store
     config = ''
       f6 : open -a "${pkgs.brave}/Applications/Brave Browser.app"
       f7 : open -a "/Applications/Ghostty.app"
-      f8 : open -a "${pkgs.slack}/Applications/Slack.app"
+      f8 : open -a "/Applications/Slack.app"
     '';
   };
 }

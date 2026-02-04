7ae6cf1b157413f3315ded50f8f70d43e08c5589
Max Netterberg <max.netterberg@gmail.com>
2026-02-04 11:56:37 +0100

    add better config for workmux

--- home-manager/programs/workmux/default.nix
+++ home-manager/programs/workmux/default.nix
@@ -6,12 +6,14 @@
   home.packages = [workmux.packages.${pkgs.system}.default];
 
   xdg.configFile."workmux/config.yaml".text = ''
+    post_create:
+      - direnv allow
     nerdfont: true
     merge_strategy: rebase
     agent: claude
     panes:
       - command: <agent>
         focus: true
-      - split: horizontal
+        split: horizontal
   '';
 }

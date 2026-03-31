b56f2bc03dda402d0e97557f2b1b34134ec4410f
Max Netterberg <max.netterberg@gmail.com>
2026-03-31 16:09:44 +0200

    add instructions to liag

--- home-manager/scripts/bin/linear_agent.fish
+++ home-manager/scripts/bin/linear_agent.fish
@@ -2,4 +2,8 @@
 
 set ticket_id $argv[1]
 
-workmux add -A -p "$(linear $ticket_id)"
+workmux add -A -p "Fix this ticket:
+
+$(linear $ticket_id)
+
+When you are finished create a PR with the github cli and request review from philip@normain.com"

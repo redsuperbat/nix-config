f0e7d98bf5c7f051c1a03cb0904c7c36ab4b9ecf
Max Netterberg <max.netterberg@gmail.com>
2026-04-02 17:19:09 +0200

    fix treesitter

--- home-manager/programs/neovim/nvim/lua/max/plugins/treesitter.lua
+++ home-manager/programs/neovim/nvim/lua/max/plugins/treesitter.lua
@@ -1,63 +1,32 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  version = false,
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  keys = {
-    {
-      "<C-space>",
-      function()
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    version = false,
+    lazy = false,
+  },
+  {
+    "lewis6991/ts-install.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    lazy = false,
+    config = function()
+      require("ts-install").setup({
+        auto_install = true,
+      })
+
+      vim.keymap.set({ "n", "x" }, "<C-space>", function()
         local select = require("vim.treesitter._select")
         if vim.fn.mode() == "n" then
           vim.cmd("normal! v")
         end
         select.select_parent(vim.v.count1)
-      end,
-      desc = "Increment Selection",
-      mode = { "n", "x" },
-    },
-    {
-      "<bs>",
-      function()
+      end, { desc = "Increment Selection" })
+
+      vim.keymap.set("x", "<bs>", function()
         require("vim.treesitter._select").select_child(vim.v.count1)
-      end,
-      desc = "Decrement Selection",
-      mode = "x",
-    },
+      end, { desc = "Decrement Selection" })
+    end,
   },
-  config = function()
-    require("nvim-treesitter").setup({
-      auto_install = true,
-      ensure_installed = {
-        "bash",
-        "diff",
-        "html",
-        "rust",
-        "javascript",
-        "jsdoc",
-        "json",
-        "terraform",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-    })
-  end,
 }

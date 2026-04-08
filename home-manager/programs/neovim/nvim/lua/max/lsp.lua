3db14ca48c667d7cf7c962c32624666e3631218d
Max Netterberg <max.netterberg@gmail.com>
2026-04-08 09:45:16 +0200

    remove built in lsp commands

--- home-manager/programs/neovim/nvim/lua/max/lsp.lua
+++ home-manager/programs/neovim/nvim/lua/max/lsp.lua
@@ -19,23 +19,7 @@ vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", {
   desc = "Language server info",
 })
 
-vim.api.nvim_create_user_command("LspStart", function(ctx)
-  local config = vim.lsp.config[ctx.args]
-  vim.lsp.start(config)
-end, { desc = "Start lsp with name", nargs = 1 })
-
-vim.api.nvim_create_user_command("LspStop", function(ctx)
-  vim.lsp.stop_client(vim.lsp.get_clients({ name = ctx.args }))
-end, { desc = "Stop lsp client", nargs = 1 })
-
-vim.api.nvim_create_user_command("LspRestart", function()
-  vim.lsp.stop_client(vim.lsp.get_clients())
-  vim.defer_fn(function()
-    vim.cmd("edit")
-  end, 1000)
-end, { desc = "Restart all language servers" })
-
-vim.keymap.set("n", "<leader>lr", ":LspRestart<CR>", { desc = "Restart LSP" })
+vim.keymap.set("n", "<leader>lr", ":lsp restart<CR>", { desc = "Restart LSP" })
 vim.keymap.set("n", "<leader>li", ":LspInfo<CR>", { desc = "LSP Info" })
 vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })

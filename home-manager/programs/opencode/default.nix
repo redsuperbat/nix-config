107030f14faadaae0f81f8a9e0e8d047cef2f050
Max Netterberg <max.netterberg@gmail.com>
2026-04-15 15:18:26 +0200

    fix the opencode tui config

--- home-manager/programs/opencode/default.nix
+++ home-manager/programs/opencode/default.nix
@@ -1,4 +1,11 @@
-{...}: {
+{
+  config,
+  configDir,
+  ...
+}: {
   programs.opencode.enable = true;
-  xdg.configFile."opencode/config.json".source = ./opencode-config.json;
+  xdg.configFile."opencode/config.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${configDir}/nix-config/home-manager/programs/opencode/opencode-config.json";
+  xdg.configFile."opencode/tui.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${configDir}/nix-config/home-manager/programs/opencode/opencode-tui.json";
 }

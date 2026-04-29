7b62db442e29fc2b7e521ea2e1d81ad891240c3b
Max Netterberg <max.netterberg@gmail.com>
2026-04-29 11:30:31 +0200

    helium open

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -3,7 +3,7 @@
     enable = pkgs.stdenv.isDarwin;
     # Some programs are installed with homebrew and therefore we cannot reference them through the store
     config = ''
-      f6 : open -a "${pkgs.brave}/Applications/Brave Browser.app"
+      f6 : open -a "~/Applications/Home Manager Apps/Helium.app"
       f7 : open -a "/Applications/Ghostty.app"
       f8 : open -a "/Applications/Slack.app"
     '';

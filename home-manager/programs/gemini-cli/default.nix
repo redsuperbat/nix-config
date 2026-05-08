3fe2a1268048313b920d45fe0aaadf5b1b0153ac
Max Netterberg <max.netterberg@gmail.com>
2026-05-08 08:52:08 +0200

    remove mcp servers for gemini cli

--- home-manager/programs/gemini-cli/default.nix
+++ home-manager/programs/gemini-cli/default.nix
@@ -7,19 +7,6 @@
       preferredEditor = "vim";
       selectedAuthType = "oauth-personal";
       usageStatisticsEnabled = false;
-      mcpServers = {
-        context7 = {
-          command = "npx";
-          args = ["-y" "@upstash/context7-mcp"];
-        };
-        github = {
-          command = "docker";
-          args = ["run" "-i" "--rm" "-e" "GITHUB_PERSONAL_ACCESS_TOKEN" "ghcr.io/github/github-mcp-server"];
-          env = {
-            GITHUB_PERSONAL_ACCESS_TOKEN = "$GITHUB_PAT";
-          };
-        };
-      };
     };
   };
 }

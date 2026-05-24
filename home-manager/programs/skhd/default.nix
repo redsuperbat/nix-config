bcd5f905b619912785dae1869d6a7e1f9033c7e8
Max Netterberg <max.netterberg@gmail.com>
2026-05-24 18:34:25 +0200

    add helium

--- home-manager/programs/skhd/default.nix
+++ home-manager/programs/skhd/default.nix
@@ -3,7 +3,7 @@
     enable = pkgs.stdenv.isDarwin;
     # Some programs are installed with homebrew and therefore we cannot reference them through the store
     config = ''
-      f6 : open -a "~/Applications/Home Manager Apps/Helium.app"
+      f6 : helium
       f7 : open -a "/Applications/Ghostty.app"
       f8 : open -a "/Applications/Slack.app"
     '';

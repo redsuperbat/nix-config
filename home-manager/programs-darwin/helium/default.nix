5ae3d96a9509c9d1bdccba12e82465d3c53093c0
Max Netterberg <max.netterberg@gmail.com>
2026-06-22 14:04:58 +0200

    do not hold to quit helium

--- home-manager/programs-darwin/helium/default.nix
+++ home-manager/programs-darwin/helium/default.nix
@@ -1,3 +1,8 @@
 {pkgs, ...}: {
   home.packages = [pkgs.helium];
+
+  # Disable the "hold ⌘Q to quit" confirmation in Helium (Chromium-based).
+  targets.darwin.defaults."net.imput.helium" = {
+    ConfirmToQuitEnabled = false;
+  };
 }

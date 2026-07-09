4b0819e2808a036359f4dd3b3097bc50e743390e
Max Netterberg <max.netterberg@gmail.com>
2026-07-09 16:25:41 +0200

    use tsc

--- home-manager/programs/neovim/nvim/lsp/tsgo.lua
+++ home-manager/programs/neovim/nvim/lsp/tsgo.lua
@@ -64,7 +64,7 @@ return {
   },
 
   cmd = function(dispatchers, config)
-    local cmd = "tsgo"
+    local cmd = "tsc"
     if (config or {}).root_dir then
       local local_cmd = vim.fs.joinpath(config.root_dir, "node_modules/.bin", cmd)
       if vim.fn.executable(local_cmd) == 1 then

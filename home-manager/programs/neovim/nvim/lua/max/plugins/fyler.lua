a0cdd330c23f847d26b7b51f082924b0fe630472
Max Netterberg <max.netterberg@gmail.com>
2026-07-25 18:21:45 +0200

    use fyler.nvim

---
+++ home-manager/programs/neovim/nvim/lua/max/plugins/fyler.lua
@@ -0,0 +1,92 @@
+--- Absolute path of the item under the cursor
+---@param instance table: Fyler finder instance
+---@return string|nil
+local function cursor_path(instance)
+  local node = require("fyler.finder").parse_cursor_line(instance)
+  if not node then
+    return nil
+  end
+  return node.link or node.path
+end
+
+--- Nearest directory to the item under the cursor
+---@param instance table: Fyler finder instance
+---@return string|nil
+local function cursor_dir(instance)
+  local node = require("fyler.finder").parse_cursor_line(instance)
+  if not node then
+    return nil
+  end
+  local path = node.link or node.path
+  return node.type == "directory" and path or vim.fs.dirname(path)
+end
+
+---@module "lazy"
+---@type LazySpec
+return {
+  "FylerOrg/fyler.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("fyler").toggle({ kind = "split_left_most", root_path = require("max.utils.fs").root() })
+      end,
+      desc = "File-tree explorer",
+    },
+    {
+      "<leader>E",
+      function()
+        require("fyler").toggle({ kind = "split_left_most", root_path = vim.fn.getcwd() })
+      end,
+      desc = "File-tree explorer (cwd)",
+    },
+  },
+  opts = {
+    hooks = {
+      on_rename = function(src_path, destination_path)
+        require("snacks").rename.on_rename_file(src_path, destination_path)
+      end,
+    },
+    mappings = {
+      n = {
+        ["S"] = {
+          action = function(instance)
+            local path = cursor_dir(instance)
+            if not path then
+              return
+            end
+            require("snacks").picker.grep({ cwd = path })
+          end,
+        },
+        ["F"] = {
+          action = function(instance)
+            local path = cursor_dir(instance)
+            if not path then
+              return
+            end
+            require("snacks").picker.files({ cwd = path })
+          end,
+        },
+        ["Y"] = {
+          action = function(instance)
+            local path = cursor_path(instance)
+            if not path then
+              return
+            end
+            vim.fn.setreg("+", path, "c")
+            vim.print("copied!")
+          end,
+        },
+        ["O"] = {
+          action = function(instance)
+            local path = cursor_path(instance)
+            if not path then
+              return
+            end
+            os.execute("open -R " .. vim.fn.shellescape(path))
+          end,
+        },
+      },
+    },
+  },
+}

db29ad0b99b03accadd04e7a24235acec2eeb5e1
Max Netterberg <max.netterberg@gmail.com>
2026-08-08 18:58:54 +0200

    Revert "do not open in fullscreen mode"
    This reverts commit ac6d409320f48e09bc00eb1845d71e883c0e1c2e.

--- home-manager/programs-linux/nmtui/default.nix
+++ home-manager/programs-linux/nmtui/default.nix
@@ -3,7 +3,7 @@
     name = "nmtui";
     comment = "NetworkManager text user interface";
     icon = "network-wired";
-    exec = "${pkgs.ghostty}/bin/ghostty --title=nmtui-wifi --config=fullscreen=false -e ${pkgs.networkmanager}/bin/nmtui";
+    exec = "${pkgs.ghostty}/bin/ghostty --title=nmtui-wifi -e ${pkgs.networkmanager}/bin/nmtui";
     categories = ["System" "Network"];
     settings.Keywords = "wifi;network;wireless";
   };

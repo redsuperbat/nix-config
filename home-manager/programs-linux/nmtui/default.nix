4f66c6c6b0a417c1475384cf7331ae02017b7587
Max Netterberg <max.netterberg@gmail.com>
2026-08-08 18:53:56 +0200

    small fixes

--- home-manager/programs-linux/nmtui/default.nix
+++ home-manager/programs-linux/nmtui/default.nix
@@ -3,9 +3,8 @@
     name = "nmtui";
     comment = "NetworkManager text user interface";
     icon = "network-wired";
-    exec = "${pkgs.ghostty}/bin/ghostty -e ${pkgs.networkmanager}/bin/nmtui";
+    exec = "${pkgs.ghostty}/bin/ghostty --title=nmtui-wifi -e ${pkgs.networkmanager}/bin/nmtui";
     categories = ["System" "Network"];
     settings.Keywords = "wifi;network;wireless";
   };
 }
-

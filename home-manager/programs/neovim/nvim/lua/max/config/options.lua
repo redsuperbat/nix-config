058792fc0d1167f7e176afaf2f35b85179aa8fb1
Max Netterberg <max.netterberg@gmail.com>
2026-09-17 09:42:16 +0200

    add listchars options

--- home-manager/programs/neovim/nvim/lua/max/config/options.lua
+++ home-manager/programs/neovim/nvim/lua/max/config/options.lua
@@ -27,6 +27,7 @@ vim.opt.ignorecase = true -- Ignore case
 vim.opt.inccommand = "nosplit" -- preview incremental substitute
 vim.opt.laststatus = 3 -- global statusline
 vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.listchars = { tab = "  ", trail = "-", nbsp = "+" }
 vim.opt.mouse = "a" -- Enable mouse mode
 vim.opt.number = true -- Print line number
 vim.opt.pumblend = 10 -- Popup blend
